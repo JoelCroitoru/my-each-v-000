5b8fe0ca220b64c08467e315f1f3e4f1918546ae
Joel Croitoru <jcroitoru@gmail.com>
2020-07-22 16:09:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each(collection)
   if block_given?
     i=0
     while i<collection.length
-      yield collection[i]
-      puts i
+      yield (collection[i])
+      i
       i=i+1
     end
     collection
